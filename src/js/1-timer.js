4dfdc58ae506b9a23012b161e990272b4721ede4
Denys Tryshkin <denystryshkin@gmail.com>
2025-03-25 23:31:01 -0400

    done

---
+++ src/js/1-timer.js
@@ -0,0 +1,92 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { refsTimer } from './refs.js';
+
+const { input, days, hours, minutes, seconds, btnStart } = refsTimer;
+
+let userSelectedDate = null;
+let interval = null;
+
+flatpickr(input, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectDate = selectedDates[0];
+
+    if (selectDate < new Date()) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+
+      btnStart.setAttribute('disabled', 'true');
+    } else {
+      userSelectedDate = selectDate;
+      btnStart.removeAttribute('disabled');
+    }
+  },
+});
+
+btnStart.addEventListener('click', () => {
+  start();
+});
+
+function start() {
+  interval = setInterval(() => {
+    const diff = userSelectedDate - Date.now();
+    btnStart.setAttribute('disabled', '');
+    input.setAttribute('disabled', '');
+
+    if (diff < 0) {
+      input.removeAttribute('disabled');
+      btnStart.removeAttribute('disabled');
+
+      stop();
+      return;
+    }
+
+    const timeComponent = convertMs(diff);
+
+    days.textContent = pad(timeComponent.days);
+    hours.textContent = pad(timeComponent.hours);
+    minutes.textContent = pad(timeComponent.minutes);
+    seconds.textContent = pad(timeComponent.seconds);
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(interval);
+
+  days.textContent = '00';
+  hours.textContent = '00';
+  minutes.textContent = '00';
+  seconds.textContent = '00';
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { refsSnackbar } from './refs';
+
+const { form, delay } = refsSnackbar;
+let delayTime = null;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  delayTime = parseInt(delay.value) > 0 ? parseInt(delay.value) : 0;
+
+  const selectedState = document.querySelector('[name="state"]:checked').value;
+
+  simulatePromise(selectedState, delayTime)
+    .then(message => {
+      iziToast.success({
+        message,
+        position: 'topRight',
+      });
+    })
+    .catch(message => {
+      iziToast.error({
+        message,
+        position: 'topRight',
+      });
+    });
+});
+
+function simulatePromise(state, daleyValue) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(message(state, daleyValue));
+      } else {
+        reject(message(state, daleyValue));
+      }
+    }, delayTime);
+  });
+}
+
+function message(stateValue, delaySet) {
+  return `${stateValue} promise in ${delaySet} ms`;
+}
